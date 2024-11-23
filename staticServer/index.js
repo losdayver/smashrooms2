8b686348c749c7d44c5051d8aef82743172a0779
losdayver <losdayver@yandex.ru>
2024-11-23 12:49:48 +0300

    client boilerplate, static server

---
+++ staticServer/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const staticRoot = "../static/";
+
+app.use(express.json());
+app.use(express.static(staticRoot));
+
+app.set("styles", path.resolve(__dirname + staticRoot + "styles"));
+app.set("img", path.resolve(__dirname + staticRoot + "img"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, staticRoot, "html", "index.html"));
+});
+
+app.listen(5890, "0.0.0.0", () => {
+  console.log(`static server listening on port ${5890}!`);
+});
