0ca4a30bc4a49bb7b9dfc99e1c0316ec1305d744
losdayver <losdayver@yandex.ru>
2024-12-01 17:00:02 +0300

    fix win build, added build for sh

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-npm i -g typescript & cd server & npm i & tsc & cd ..\client & npm i & tsc
+npm i -g typescript & cd server & npm i & tsc & cd ..\client & npm i & tsc & cd ..\staticServer & npm i
---
+++ build.sh
@@ -0,0 +1,9 @@
+npm i -g typescript
+cd server
+npm i
+tsc
+cd ../client
+npm i
+tsc
+cd ../staticServer
+npm i
