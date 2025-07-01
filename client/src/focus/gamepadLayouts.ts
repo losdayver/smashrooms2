a932d4a003661b1d3ff16734636fe6a8c455a9db
pursvir <i-have@no.email>
2025-07-02 01:56:53 +0300

    Fixed poor performance in Chromium browsers

--- client/src/focus/gamepadLayouts.ts
+++ client/src/focus/gamepadLayouts.ts
@@ -1,22 +1,18 @@
-// TODO: not sure if it should be in separate file here, or in config, or in focusManager.ts..
-
 const axisActivationThreshold: number = 0.5;
-// TODO: split this map into playStationBtnToCtrlKeyMap and xBoxBtnToCtrlKeyMap
-//       or just create aliases
-// TODO: Xbox -> Connect, Logitech -> Mode btns control options
+
 export const gamepadEventToKeyMap: GamepadControlsObjType = {
-  Cross: (gp) => gp.buttons[0].pressed, // XBox -> A
-  Circle: (gp) => gp.buttons[1].pressed, // XBox -> B
-  Square: (gp) => gp.buttons[2].pressed, // XBox -> X
-  Triangle: (gp) => gp.buttons[3].pressed, // XBox -> Y
-  // TODO: different naming in non-PS gamepads: 1 => bumper, 2 => trigger
+  Cross: (gp) => gp.buttons[0].pressed, // A for XBox
+  Circle: (gp) => gp.buttons[1].pressed, // B for XBox
+  Square: (gp) => gp.buttons[2].pressed, // X for XBox
+  Triangle: (gp) => gp.buttons[3].pressed, // Y for XBox
+  // Different naming for non-PS gamepads: bumper instead of 1, trigger instead of 2
   L1: (gp) => gp.buttons[4].pressed,
   R1: (gp) => gp.buttons[5].pressed,
   L2: (gp) => gp.buttons[6].pressed,
   R2: (gp) => gp.buttons[7].pressed,
   // or "Create", according to PS5 specs
-  Share: (gp) => gp.buttons[8].pressed, // Xbox -> Back
-  Options: (gp) => gp.buttons[9].pressed, // XBox -> Start
+  Share: (gp) => gp.buttons[8].pressed, // Back for XBox
+  Options: (gp) => gp.buttons[9].pressed, // Start for XBox
   LeftAnalog: (gp) => gp.buttons[10].pressed,
   RightAnalog: (gp) => gp.buttons[11].pressed,
   DPadUp: (gp) => gp.buttons[12].pressed,
@@ -33,9 +29,9 @@ export const gamepadEventToKeyMap: GamepadControlsObjType = {
   RStickUp: (gp) => gp.axes[3] < -axisActivationThreshold,
   RStickDown: (gp) => gp.axes[3] > axisActivationThreshold,
   RStickLeft: (gp) => gp.axes[2] < -axisActivationThreshold,
-  // TODO: use L2, R2 (indexes 4, 5) activation thresholds?
+  // TODO: will we use L2, R2 (indexes 4, 5) activation thresholds?
 } as const;
 
 export type GamepadControlsObjType = Record<string, (gp: Gamepad) => boolean>;
-// TODO: more?
-type GamepadLayout = "PS" | "XBox";
+
+// TODO: gamepad layouts (e.g. PS4, PS3, XBox, ...)
