bcf1037d4d0a60f541348f275341dcc67397462e
losdayver <losdayver@yandex.ru>
2025-09-28 16:02:41 +0300

    script fix

--- scripts/run-in-tmux.sh
+++ scripts/run-in-tmux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-tmux new-session './run-ws-server.bash' \; \
-     split-window './run-static-server.bash'
+tmux new-session './run-ws-server.sh' \; \
+     split-window './run-static-server.sh'
